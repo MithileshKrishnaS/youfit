2e4a3916dbc003eaf6320de029105f248dcec718
MithileshKrishnaS <mithileshkrishna20@gmail.com>
2022-03-01 15:22:21 +0530

    last commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import Home from './components/Home';
+import Each from './components/Each';
+import {BrowserRouter, Route}from 'react-router-dom';
+import { Routes } from 'react-router';
+import { useState } from 'react';
+
+var a='wkfn'
+function App() {
+
+  const[index,setIndex]=useState();
+
+  const handleIndex=(data)=>
+  {
+    setIndex(data)
+  }
+
+  return (
+    <div className="App">
+      <div className="nav">
+        {/* <img src={logo1}></img> */}
+        <i class="fas fa-dumbbell fa-4x"></i>
+        <span>Youfit</span>
+      </div>     
+       <BrowserRouter>
+          <Routes>
+            <Route path="/youfit" exact  element={<Home data={handleIndex}></Home>}></Route>
+            <Route path="/each/:id" exact element={<Each data={index}></Each>}></Route>
+          </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
